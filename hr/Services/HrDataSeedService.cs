119f1769ed70b7c5c08fbce4090b9a6a51c8dd1b
saqibrazzaq <saqibrazzaq@gmail.com>
2022-07-17 03:09:46 +0500

    seed gender

--- hr/Services/HrDataSeedService.cs
+++ hr/Services/HrDataSeedService.cs
@@ -1,4 +1,6 @@
-﻿using logger;
+﻿using hr.Entities;
+using hr.Repository;
+using logger;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +12,31 @@ namespace hr.Services
     public class HrDataSeedService : IHrDataSeedService
     {
         private readonly ILoggerManager _logger;
+        private readonly IHrRepositoryManager _repositoryManager;
 
-        public HrDataSeedService(ILoggerManager logger)
+        public HrDataSeedService(ILoggerManager logger, 
+            IHrRepositoryManager repositoryManager)
         {
             _logger = logger;
+            _repositoryManager = repositoryManager;
         }
 
         public void Seed()
         {
-            _logger.LogInfo("No seed data for Hr.");
+            _logger.LogInfo("Seeding data for hr database...");
+            SeedGender();
+            _logger.LogInfo("Finished.");
+        }
+
+        private void SeedGender()
+        {
+            var count = _repositoryManager.GenderRepository.FindAll(false).Count();
+            if (count == 0)
+            {
+                _repositoryManager.GenderRepository.Create(new Gender { GenderCode = 'M', Name = "Male" });
+                _repositoryManager.GenderRepository.Create(new Gender { GenderCode = 'F', Name = "Female" });
+                _repositoryManager.Save();
+            }
         }
     }
 }
